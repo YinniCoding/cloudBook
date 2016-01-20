27364d01f8b657b6420fa709d5efe6cb76e4350c
zhangjh <njhxzhangjihong@126.com>
2016-01-20 15:18:16 +0800

    剩余页面的制作

---
+++ js/finace.js
@@ -0,0 +1,323 @@
+/**Des: 财务中心页js文件
+ * Author：njhxzhangjihong@126.com
+ * Date：2016/1/12
+ */
+//时间框日期设置
+function setDate(){
+    var d = new Date();
+    var year = d.getFullYear();
+    var month = d.getMonth() + 1;
+    if(month < 10){
+        month = "0" + month;
+    }
+    var day = d.getDate();
+    $("#datetimepicker").val(year + "-" + month + "-" + day);
+}
+
+/*设置分页器
+* Params:
+ * page:当前页
+ * total：一共多少条记录
+ * rows：每页多少条记录
+* */
+function setPagination(obj){
+    var page = obj.page;
+    var total = obj.total;
+    var rows = obj.rows;
+    var totalPage = Math.ceil(total / rows);
+    var liContent = "";
+
+    //控制翻页器一共多长(1...6,7,8,9,10...totalPage)
+    var start = 0;
+    var end = 5;
+    //如果条数不够五页，则以实际页数为准
+    if(page > 5){
+        start = page - 2;
+        end = page + 2;
+    }
+    end = (totalPage < end) ? totalPage : end;
+    if(start !== 0){
+        //第一页必须保留
+        liContent += "<li><a href='javascript:;'>1</a></li>";
+        liContent += "<li class='disabled'><a href='javascript:;'>...</a></li>";
+    }
+    for(var j=start;j<end;j++){
+        liContent += "<li><a href='javascript:;'>" + (j+1) + "</a></li>"
+    }
+    if(totalPage > 5 && end !== totalPage){
+        //页码较多时最末页必须保留
+        liContent += "<li class='disabled'><a href='javascript:;'>...</a></li>";
+        liContent += "<li><a href='javascript:;'>" + totalPage + "</a></li>";
+    }
+    $("#totalNum").text(total);
+    $("#paginationPages > li:first-child").nextAll().each(function(index,ele){
+        if(!/next/.test($(ele).attr("class"))){
+            $(ele).remove();
+        }
+    });
+    $("#paginationPages > li:first-child").after(liContent);
+
+    definePageClick(obj);
+}
+
+/*设置当前页码高亮
+ * params:
+ * page: 当前页码
+ * totalPage: 总页码
+* */
+function setActive(page,totalPage){
+    $("#paginationPages > li > a").each(function (index, ele) {
+        if($(ele).text() == page){
+            $("#paginationPages > li.active").removeClass("active");
+            $(ele).parent().addClass("active");
+            $("#paginationPages .previous").removeClass("disabled");
+            $("#paginationPages .next").removeClass("disabled");
+        }
+    });
+    if(page == 1){
+        $("#paginationPages .previous").addClass("disabled");
+    }
+    if(page == totalPage){
+        $("#paginationPages .next").addClass("disabled");
+    }
+}
+
+
+/*
+ * 获取分页数据
+ * params：json
+ * page: 页码
+ * rows：每页多少条记录
+ * userInfo：用户id
+ * 其余为可选筛选参数
+ * */
+function getData(obj){
+    $.ajax({
+        url: "http://test1.qess.me/ceo/getAssignmentList.htm",
+        data: obj,
+        beforeSend: function () {
+            $("#loading").show();
+        },
+        success: function (ret) {
+            $("#loading").hide();
+            var ret = JSON.parse(ret);
+            var res = ret.result;
+            var tradeNumber,addTime,name,phone,address,message,inviteCode,remarkInfo;
+            var total = res.total;
+            var totalPage = Math.ceil(total / obj.rows);
+            var trContent = "<tr>";
+            if(ret.code !== -1){
+                for(var i in res.rows){
+                    tradeNumber = res.rows[i].order.tradeNumber;
+                    addTime = res.rows[i].addTime;
+                    name = res.rows[i].order.name;
+                    phone = res.rows[i].order.phone;
+                    address = res.rows[i].order.address;
+                    message = res.rows[i].order.message;
+                    remarkInfo = res.rows[i].remarkInfo;
+                    inviteCode = "";
+                    trContent += "<td>" + tradeNumber + "</td>";
+                    trContent += "<td>" + addTime + "</td>";
+                    trContent += "<td>" + name + "</td>";
+                    trContent += "<td>" + phone + "</td>";
+                    trContent += "<td>" + address + "</td>";
+                    trContent += "<td>" + message + "</td>";
+                    trContent += "<td>" + inviteCode + "</td>";
+                    switch(remarkInfo){
+                        case "1":
+                            trContent += "<td class='order'>未出库</td>";
+                            break;
+                        case "2":
+                            trContent += "<td class='order'>待配送<span class='glyphicon glyphicon-edit'></span></td>";
+                            break;
+                        case "3":
+                            trContent += "<td class='order'>电话不通<span class='glyphicon glyphicon-edit'></span></td>";
+                            break;
+                        case "4":
+                            trContent += "<td class='order'>配送完成</td>";
+                            break;
+                        case "5":
+                            trContent += "<td class='order'>订单错误<span class='glyphicon glyphicon-edit'></span></td>td>";
+                            break;
+                        case "6":
+                            trContent += "<td class='order'>不在寝室<span class='glyphicon glyphicon-edit'></span></td>td>";
+                            break;
+                    }
+                    trContent += "</tr>";
+                }
+                $("#dataTable > tbody").html(trContent);
+                changeColor();
+                setPagination({
+                    page:obj.page,
+                    total:total,
+                    rows:obj.rows,
+                    userInfo:obj.userInfo,
+                    addTime:obj.addTime,
+                    remarkInfo:obj.remarkInfo,
+                    name:obj.name
+                });
+                setActive(obj.page,totalPage);
+                //definePageClick(obj);
+            }else {
+                //异常
+                alert("请求出错：" + ret.msg);
+            }
+        }
+    });
+}
+
+/*
+* 定义页码点击事件
+* params:
+* page: 当前页码 必选
+* 其余为筛选参数
+* */
+function definePageClick(dataObj){
+    var page,curPage;
+    $("#paginationPages > li > a").each(function (index, ele) {
+        var className = $(ele).parent().attr("class");
+        if(!className){
+            $(ele).on("click",function () {
+                //非前翻页后翻页的普通翻页元素
+                page = parseInt($(ele).text());
+                dataObj.page = page;
+                $(ele).unbind("click");
+                getData(dataObj);
+            });
+        }
+    });
+
+    $("#paginationPages > li.previous > a").unbind("click");
+    $("#paginationPages > li.next > a").unbind("click");
+    $("#paginationPages > li.previous > a").on("click", function () {
+        if(!/disabled/.test($(this).parent().attr("class"))){
+            curPage = $("#paginationPages > li.active > a").text();
+            page = curPage - 1;
+            //page = page <= 0 ? 1 : page;
+            dataObj.page = page;
+            getData(dataObj);
+        }
+    });
+
+    $("#paginationPages > li.next > a").on("click", function () {
+        if(!/disabled/.test($(this).parent().attr("class"))){
+            curPage = $("#paginationPages > li.active > a").text();
+            page = parseInt(curPage) + 1;
+            //page = page >= totalPage ?
+            dataObj.page = page;
+            getData(dataObj);
+        }
+    });
+}
+
+//筛选
+function filter() {
+    $("#filter").on("click",function () {
+        var date = $("#datetimepicker").val();
+        var orderStatus = $("#orderStatus option:selected").val();
+        var query = $("#query").val();
+        var dataObj = {};
+        dataObj.page = 1;
+        dataObj.rows = 16;
+        dataObj.userInfo = 42;
+        if(date){
+            dataObj.addTime = date;
+        }
+        if(orderStatus){
+            dataObj.remarkInfo = orderStatus;
+        }
+        if(query){
+            //电话
+            if(/[0-9]{11}/.test(query)){
+                dataObj.phone = query;
+            }else {
+                dataObj.name = query;
+            }
+        }
+        getData(dataObj);
+    });
+}
+
+//左侧菜单点击切换
+function sidebar(obj) {
+    $("#sidebar li > a").each(function (index, ele) {
+        $(ele).on("click", function () {
+            var className = $(ele).attr("class");
+            switch (className){
+                case "menu_0":
+                    obj.remarkInfo = "";
+                    getData(obj);
+                    break;
+                case "menu_1":
+                    obj.remarkInfo = 1;
+                    getData(obj);
+                    break;
+                case "menu_2":
+                    obj.remarkInfo = 2;
+                    getData(obj);
+                    break;
+            }
+        });
+    });
+}
+
+function getWageTotal(userInfo) {
+    var total = 0;
+    var balances = 0;
+    var res;
+    $.ajax({
+        url: "http://test1.qess.me/ceo/getWageTotal.htm",
+        data: {userInfo:userInfo},
+        success: function (ret) {
+            ret = JSON.parse(ret);
+            res = ret.result;
+            if(!ret.code){
+                total = res.summary;
+                balances = res.balances;
+                $("#totalMoney").text(total);
+                $("#withdrawCash").text(balances);
+                adjustMargin();
+            }
+        },
+        error: function (){
+
+        }
+    });
+}
+
+//动态调整工资文案的对齐
+function adjustMargin() {
+    var totalW = $("#totalMoney").width();
+    var withdrawW = $("#withdrawCash").width();
+    var textWrapW = $("#totalMoney").parents().find(".caption").width();
+    var totalmarginW = (textWrapW - totalW) / 2;
+    var withdrawmarginW = (textWrapW - withdrawW) / 2;
+    $("#totalMoney").css({"margin-left": totalmarginW});
+    $("#withdrawCash").css({"margin-left": withdrawmarginW});
+}
+
+//浮动特效
+(function (){
+    $("#salaryApplyContent > div").hover(function () {
+        $(this).addClass("shadow");
+    }, function () {
+        $(this).removeClass("shadow");
+    });
+})();
+
+(function main() {
+    setDate();
+    //默认展示第一页，每页16条，待配送状态
+    //var page = 1;
+    //var rows = 16;
+    //var userInfo = 42;
+    //var orderStatus = 2;
+    ////TODO: userInfo需要修改为实际的
+    //getData({page:page,rows:rows,userInfo:userInfo,remarkInfo:orderStatus});
+    //filter();
+    //sidebar({page:page,rows:rows,userInfo:userInfo});
+
+    getWageTotal(42);
+
+})();
+
