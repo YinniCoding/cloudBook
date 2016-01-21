8b7ee3a2664ad2f26360115c5677ef3300925500
zhangjh <njhxzhangjihong@126.com>
2016-01-21 11:47:27 +0800

    公共函数抽取+收书数据填充

--- js/summary.js
+++ js/summary.js
@@ -2,6 +2,26 @@
  * Author：njhxzhangjihong@126.com
  * Date：2016/1/13.
  */
+function getSummaryInfo(userInfo) {
+    $.ajax({
+        url: "http://test1.qess.me/ceo/getIndexInfo.htm",
+        data: {userInfo: userInfo}
+    }).done(function (ret) {
+        ret = JSON.parse(ret);
+        var res = ret.result;
+        if(!ret.code){
+            var NotDispatchingCount,NotExWarehouseCount,balances;
+            NotDispatchingCount = res.NotDispatchingCount;
+            NotExWarehouseCount = res.NotExWarehouseCount;
+            balances = res.balances;
+            $("#waitForSend").text(NotDispatchingCount);
+            $("#waitForPop").text(NotExWarehouseCount);
+            $("#salary").text(balances);
+        }
+    }).fail(function () {
+        alert("请求失败");
+    });
+}
 
 (function () {
     $("#info div").on("mouseover", function () {
@@ -12,4 +32,7 @@
     });
     $(".banner").luara({height:"100%",interval:4500,selected:"seleted",deriction:"left"});
     //$("#banner").luara({width:"500",height:"334",interval:4500,selected:"seleted",deriction:"left"});
+
+    var userInfo = 42;
+    getSummaryInfo(userInfo);
 })();
--- js/tuiguang.js
+++ js/tuiguang.js
@@ -20,68 +20,6 @@ function setDate(){
  * total：一共多少条记录
  * rows：每页多少条记录
 * */
-function setPagination(obj){
-    var page = obj.page;
-    var total = obj.total;
-    var rows = obj.rows;
-    var totalPage = Math.ceil(total / rows);
-    var liContent = "";
-
-    //控制翻页器一共多长(1...6,7,8,9,10...totalPage)
-    var start = 0;
-    var end = 5;
-    //如果条数不够五页，则以实际页数为准
-    if(page > 5){
-        start = page - 2;
-        end = page + 2;
-    }
-    end = (totalPage < end) ? totalPage : end;
-    if(start !== 0){
-        //第一页必须保留
-        liContent += "<li><a href='javascript:;'>1</a></li>";
-        liContent += "<li class='disabled'><a href='javascript:;'>...</a></li>";
-    }
-    for(var j=start;j<end;j++){
-        liContent += "<li><a href='javascript:;'>" + (j+1) + "</a></li>"
-    }
-    if(totalPage > 5 && end !== totalPage){
-        //页码较多时最末页必须保留
-        liContent += "<li class='disabled'><a href='javascript:;'>...</a></li>";
-        liContent += "<li><a href='javascript:;'>" + totalPage + "</a></li>";
-    }
-    $("#totalNum").text(total);
-    $("#paginationPages > li:first-child").nextAll().each(function(index,ele){
-        if(!/next/.test($(ele).attr("class"))){
-            $(ele).remove();
-        }
-    });
-    $("#paginationPages > li:first-child").after(liContent);
-
-    definePageClick(obj);
-}
-
-/*设置当前页码高亮
- * params:
- * page: 当前页码
- * totalPage: 总页码
-* */
-function setActive(page,totalPage){
-    $("#paginationPages > li > a").each(function (index, ele) {
-        if($(ele).text() == page){
-            $("#paginationPages > li.active").removeClass("active");
-            $(ele).parent().addClass("active");
-            $("#paginationPages .previous").removeClass("disabled");
-            $("#paginationPages .next").removeClass("disabled");
-        }
-    });
-    if(page == 1){
-        $("#paginationPages .previous").addClass("disabled");
-    }
-    if(page == totalPage){
-        $("#paginationPages .next").addClass("disabled");
-    }
-}
-
 
 /*
  * 获取分页数据
@@ -162,50 +100,6 @@ function getData(obj){
                 //异常
                 alert("请求出错：" + ret.msg);
             }
-        }
-    });
-}
-
-/*
-* 定义页码点击事件
-* params:
-* page: 当前页码 必选
-* 其余为筛选参数
-* */
-function definePageClick(dataObj){
-    var page,curPage;
-    $("#paginationPages > li > a").each(function (index, ele) {
-        var className = $(ele).parent().attr("class");
-        if(!className){
-            $(ele).on("click",function () {
-                //非前翻页后翻页的普通翻页元素
-                page = parseInt($(ele).text());
-                dataObj.page = page;
-                $(ele).unbind("click");
-                getData(dataObj);
-            });
-        }
-    });
-
-    $("#paginationPages > li.previous > a").unbind("click");
-    $("#paginationPages > li.next > a").unbind("click");
-    $("#paginationPages > li.previous > a").on("click", function () {
-        if(!/disabled/.test($(this).parent().attr("class"))){
-            curPage = $("#paginationPages > li.active > a").text();
-            page = curPage - 1;
-            //page = page <= 0 ? 1 : page;
-            dataObj.page = page;
-            getData(dataObj);
-        }
-    });
-
-    $("#paginationPages > li.next > a").on("click", function () {
-        if(!/disabled/.test($(this).parent().attr("class"))){
-            curPage = $("#paginationPages > li.active > a").text();
-            page = parseInt(curPage) + 1;
-            //page = page >= totalPage ?
-            dataObj.page = page;
-            getData(dataObj);
         }
     });
 }
