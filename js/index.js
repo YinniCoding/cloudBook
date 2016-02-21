ce5619a471c0d7ebcbe9446ef00cac2454569f93
jihong.zjh <jihong.zjh@alibaba-inc.com>
2016-02-21 14:52:30 +0800

    增加回车登录事件

--- js/index.js
+++ js/index.js
@@ -13,27 +13,40 @@ var global = {
     domain: "http://ceo.qess.me"
 };
 
-(function () {
-    $("#loginBtn").on("click", function () {
-        var loginName = $("#userName").val();
-        var passwd = $("input[name='passwd']").val();
-        if(!loginName || !passwd){
-            alert("请输入用户名、密码!");
-        }else {
-            $.ajax({
-                type: "POST",
-                url: global.domain + "/ceo/userLogin.htm",
-                data: {loginName: $("#userName").val(),password: $("input[name='passwd']").val(),veriCode:$("#codeInput").val()},
-                success: function (ret) {
-                    ret = JSON.parse(ret);
-                    if(ret.code != 0){
-                        $("#loginInfo").text(ret.msg);
-                    }else {
-                        window.location.href = "summary.html";
-                    }
+function login() {
+    var loginName = $("#userName").val();
+    var passwd = $("input[name='passwd']").val();
+    if(!loginName || !passwd){
+        alert("请输入用户名、密码!");
+    }else {
+        $.ajax({
+            type: "POST",
+            url: global.domain + "/ceo/userLogin.htm",
+            data: {loginName: $("#userName").val(),password: $("input[name='passwd']").val(),veriCode:$("#codeInput").val()},
+            success: function (ret) {
+                ret = JSON.parse(ret);
+                if(ret.code != 0){
+                    $("#loginInfo").text(ret.msg);
+                }else {
+                    window.location.href = "summary.html";
                 }
-            });
+            }
+        });
+    }
+}
+
+(function () {
+    //点击登录
+    $("#loginBtn").on("click", function () {
+        login();
+    });
+
+    //回车登录
+    $(document).on("keydown", function (e) {
+        if(e.keyCode === 13){
+            login();
         }
+
     });
 
     $("#autoLogin").on("click", function () {
